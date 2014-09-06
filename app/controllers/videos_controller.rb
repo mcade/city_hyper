08452080dcfc1c72f90c509b3fc7775eb83fa786
Prelang Builder <builder@prelang.com>
2014-09-06 05:27:52 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,7 +3,7 @@ class VideosController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_video, only: [:show, :edit, :update, :destroy]
+  before_action :set_video, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /videos
   # GET /videos.json
